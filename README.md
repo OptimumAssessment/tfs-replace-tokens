e9079741b2c8d0814df357f1b6deeaa864e913b4
Jean-Pierre Broeders <github@freelyit.nl>
2019-03-29 16:43:47 +0100

    Add NodeJS build/release task

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-tfs-replace-tokens
+# Replace Tokens Build/Release task
+This build/release task can replace tokens with real values from variables, group variables and variables from any keyvault.
+
+## Why this build/release task?
+The build/release pipelines I found are pretty cool if tokens are completly unique over all you solutions. 
+If tokens are not unique and you've just a couple of variable groups, than those tasks will not work.
+Therefor you can use this build/release task where for example you can replace token **_unique-token_** with **value-a** in pipeline A, and with **value-b** in pipeline B.
---
+++ cag-replacetokens.js
@@ -0,0 +1,74 @@
+const path = require('path');
+const tl = require('azure-pipelines-task-lib');
+const sh = require('shelljs');
+const fs = require('fs');
+const os = require('os');
+
+try {
+    var sourcePath = tl.getPathInput('sourcePath', true);
+    var targetFilePattern = tl.getInput('targetFilePattern', true);
+    const tokenRegex = tl.getInput('tokenRegex', true);
+    const replaceTokens = tl.getInput('replaceTokenList', true);
+    const replaceTokensArray = replaceTokens.replace(/\r\n/g, "\r").replace(/\n/g, "\r").split(/\r/);
+    
+    var tokenDictionary = {};
+
+    // clear leading and trailing quotes for paths with spaces
+    sourcePath = sourcePath.replace(/"/g, "");
+
+    // remove trailing slash
+    if (sourcePath.endsWith("\\") || sourcePath.endsWith("/")) {
+        sourcePath = sourcePath.substr(0, sourcePath.length - 1);
+    }
+        
+    //check if source path exists
+    tl.checkPath(sourcePath, "sourcePath");
+
+    // store the tokens and values in dictionary 
+    replaceTokensArray.forEach(token => {
+        const idx = token.indexOf(':');
+        const key = token.substr(0, idx);
+        const value = token.substr(idx + 1);
+
+        tokenDictionary[key] = value;
+    });
+
+    // create a glob removing any spurious quotes
+    if (os.platform() !== "win32") {
+        // replace \ with /
+        targetFilePattern = targetFilePattern.replace(/\\/g, "/");
+    }
+
+    var files = tl.findMatch(sourcePath, targetFilePattern);
+    if (!files || files.length === 0) {
+        var msg = `Could not find files with glob [${targetFilePattern}].`;
+        if (os.platform() !== "win32") {
+            tl.warning("No files found for pattern. Non-windows file systems are case sensitvive, so check the case of your path and file patterns.");
+        }
+        tl.setResult(tl.TaskResult.Failed, msg);
+    }
+
+    for (var file of files) {
+        console.log(`Starting token replacement in [${file}]`);
+
+        const contents = fs.readFileSync(file).toString();
+        const reg = new RegExp(tokenRegex, "g");
+                
+        var newContents = contents.replace(reg, (all,identifier) => {
+            const token = tokenDictionary[identifier];
+            if(token !== undefined) {
+                console.log(`Replace token [${identifier}] with secret value.`);
+                return token;
+            } 
+            
+            console.warn(`No secret found for token [${identifier}].`);
+            return `__${identifier}__`;
+        })
+
+        //sh.chmod(666, file);
+        fs.writeFileSync(file, newContents);
+    }
+} catch (error) {
+    console.log(error);    
+    tl.exit(1);
+}
